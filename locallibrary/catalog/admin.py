40a8a44351873c990f488496e7e976bf5167e981
KimHS0915 <remember2552@gmail.com>
2020-02-29 00:11:01 +0900

    Django admin site - challenge

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -9,9 +9,14 @@ admin.site.register(Genre)
 admin.site.register(Language)
 
 # Define the admin class
+class BookInline(admin.TabularInline):
+    model = Book
+    extra = 0
+
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BookInline]
 
 # Register the admin class with the associated model
 admin.site.register(Author, AuthorAdmin)
@@ -29,6 +34,7 @@ class BookAdmin(admin.ModelAdmin):
 # Register the Admin classes for BookInstance using the decorator
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'due_back', 'id')
     list_filter = ('status', 'due_back')
 
     fieldsets = (
