7b5da0fe548d536b5cfe83be7927f5610bd7f95b
KimHS0915 <remember2552@gmail.com>
2020-03-02 00:20:49 +0900

    User authentication and permissions

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from catalog.models import Book, Author, BookInstance, Genre
 # Create your views here.
@@ -33,15 +34,29 @@ def index(request):
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'catalog/index.html', context=context)
 
+
 class BookListView(generic.ListView):
     model = Book
     paginate_by = 10
 
+
 class BookDetailView(generic.DetailView):
     model = Book
+
 
 class AuthorListView(generic.ListView):
     model = Author
 
+
 class AuthorDetailView(generic.DetailView):
     model = Author
+
+
+class LoanedBooksByUserListView(LoginRequiredMixin,generic.ListView):
+    """Generic class-based view listing books on loan to current user."""
+    model = BookInstance
+    template_name ='catalog/bookinstance_list_borrowed_user.html'
+    paginate_by = 10
+    
+    def get_queryset(self):
+        return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
